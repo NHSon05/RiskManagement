1c321ca8b8b209a01b0f6044d37d11d527f1585f
Nguyen Hong Son <snguyenhong8@gmail.com>
2026-02-06 01:13:55 +0700

    feat: add RiskType, Info, PESTEL type

--- client/src/types/projectType.ts
+++ client/src/types/projectType.ts
@@ -1,36 +1,54 @@
-export interface  ProjectInfo{
-    id: string;
-    name: string;
-    project_level: string;
-    localtion: string;
-    funding_source: string;
-    user_role: string;
-    slug?: string;
-    // PESTEL_SWOT
-    political?: string;
-    economic?: string;
-    social?: string;
-    technology?: string;
-    environment?: string;
-    legal?: string;
-    strengths?: string;
-    weakness?: string;
-    opportunities?: string;
-    threats?: string;
+export interface Info{
+  prj_id: string;
+  prj_name: string;
+  prj_level: string;
+  prj_location: string;
+  prj_fund: string;
+  prj_role: string;
+  // PESTEL_SWOT
+  // political?: string;
+  // economic?: string;
+  // social?: string;
+  // technology?: string;
+  // environment?: string;
+  // legal?: string;
+  pestel: PestelSwot[]
+  swot: PestelSwot[]
+  // strengths?: string;
+  // weakness?: string;
+  // opportunities?: string;
+  // threats?: string;
+  prj_target: Target[]
 }
 
-export interface ProjectTarget {
-    id: string;
-    projectId: string;
-    name: string;
+
+export interface Risk {
+  id: string;
+  name: string;
+  probability: string;
+  probability_level: number;
+  impact: string;
+  impact_level: number
+  risk_level: number
+  strategy: string
+  response_plans: ResponsePlan[]
+}
+export interface Target {
+  id: string;
+  name: string;
+  risks: Risk[];
 }
 
-export interface ProjectRisk {
-    id: string;
-    projectId: string;
-    name: string;
-    category: string;
-    probability: number;
-    impact: number;
-    risk_level: string;
+export interface ResponsePlan {
+  owner: string;
+  content: string;
+}
+
+export interface PestelSwot {
+  code: string
+  label: string
+  item: PestelSwotItem[]
+}
+export interface PestelSwotItem {
+  content: string
 }
