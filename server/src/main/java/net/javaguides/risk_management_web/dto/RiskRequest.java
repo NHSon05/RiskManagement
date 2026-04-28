a1be237f3fc46bfb97c826973706553fac7090db
Nguyen Hong Son <snguyenhong8@gmail.com>
2026-04-28 22:40:12 +0700

    refactor: implement adding strategy field

--- server/src/main/java/net/javaguides/risk_management_web/dto/RiskRequest.java
+++ server/src/main/java/net/javaguides/risk_management_web/dto/RiskRequest.java
@@ -2,7 +2,10 @@ package net.javaguides.risk_management_web.dto;
 
 public class RiskRequest {
     private String name;
+    private String strategy;
 
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
+    public String getStrategy() { return strategy; }
+    public void setStrategy(String strategy) { this.strategy = strategy; }
 }
