9913c4f630f57ab5fab4d9440f7317fa33a5995c
Nguyen Hong Son <snguyenhong8@gmail.com>
2026-05-06 19:51:15 +0700

    refactor: ProjectRequest

--- server/src/main/java/net/javaguides/risk_management_web/dto/ProjectRequest.java
+++ server/src/main/java/net/javaguides/risk_management_web/dto/ProjectRequest.java
@@ -3,24 +3,49 @@ package net.javaguides.risk_management_web.dto;
 public class ProjectRequest {
 
     private String name;
-    private String prjLevel;   // Cấp công trình
-    private String location;   // Địa điểm
+    private String prjLevel; // Cấp công trình
+    private String location; // Địa điểm
     private String capital; // Nguồn vốn
-    private String role;       // Vai trò
+    private String role; // Vai trò
 
     // Getters và Setters
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public String getName() {
+        return name;
+    }
 
-    public String getPrjLevel() { return prjLevel; }
-    public void setPrjLevel(String prjLevel) { this.prjLevel = prjLevel; }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    public String getLocation() { return location; }
-    public void setLocation(String location) { this.location = location; }
+    public String getPrjLevel() {
+        return prjLevel;
+    }
 
-    public String getCapital() { return capital; }
-    public void setCapital(String capital) { this.capital = capital; }
+    public void setPrjLevel(String prjLevel) {
+        this.prjLevel = prjLevel;
+    }
 
-    public String getRole() { return role; }
-    public void setRole(String role) { this.role = role; }
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
